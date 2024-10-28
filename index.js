241e1145e015dcb79e35485e36515701d4ea5f60
Raghuvans mani <raghuvansh.mishra@algoworks.com>
2024-10-28 19:28:37 +0530

    added cors morgon and some other use dependencies

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const colors = require('colors')
+const cors = require('cors')
+const dotevn = require('dotenv')
+const morgan = require('morgan')
 const app = express();
-
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+dotevn.config()
+const PORT = process.env.PORT
 app.get('/',(req,res)=>{
     console.log("this is first method i created ")
     res.send('<h1>this is my first route that i created</h1>')
 })
-app.listen(8080,()=>{
-    console.log("server is running ")
+app.listen(PORT,()=>{
+    console.log(`server is running  at ${PORT}`)
 })
